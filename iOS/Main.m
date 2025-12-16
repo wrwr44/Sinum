b2ed8c5cab741e3b2baee80f03a31443895f1ceb
wrwr44 <143880409+wrwr44@users.noreply.github.com>
2025-12-16 20:03:56 +0400

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
+#define API_URL @"http://192.168.100.115:8080"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
